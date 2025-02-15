d2b51fb193f6b30e18fdb78f22eb2c21a615d062
Saptarshi Paul <saptarshi2027paul@gmail.com>
2025-02-15 14:40:04 +0530

    Courses Page Pushed

---
+++ src/routes/courses/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CousreGrid from './coursegrid'
+const Courses =()=> {
+  return (
+    <>
+    <h1 className="text-4xl font-bold dark:text-blue-500 ml-12 ">Top Courses</h1>
+    <CousreGrid/>
+    </>
+  )
+}
+
+export default Courses
