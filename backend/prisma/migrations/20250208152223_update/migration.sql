14894256d351a13a91c174a8983bf8411ae87dbb
Saptarshi Paul <saptarshi2027paul@gmail.com>
2025-02-15 12:00:55 +0530

    Backend Deleted
    Backend Deleted

--- backend/prisma/migrations/20250208152223_update/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "UserCredentials" ADD COLUMN     "userRole" TEXT NOT NULL DEFAULT 'student';
--- backend/utils/generatetoken.js
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken'
-const generateToken=(user)=>{
-    const token=jwt.sign({id:user.id,role:user.userRole,username:user.username},process.env.JWT_SECRET,{expiresIn:'1h'})
-    return token
-}
-
-export default generateToken
