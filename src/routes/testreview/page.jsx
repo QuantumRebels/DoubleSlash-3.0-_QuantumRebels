90585fcebe4157839482d2d1c466dfb7521d961c
Chandan-kr-dev <chandanramgar@gmail.com>
2025-02-16 10:42:26 +0530

    update

--- src/routes/testreview/page.jsx
+++ src/routes/testreview/page.jsx
@@ -18,6 +18,36 @@ const Dashboard = () => {
    
   ];
 
+  const showReport=()=>{
+    axios.post('http://localhost:5000/generate-report',{
+      "overall_performance": {
+          "Student ID": "S1",
+          "Student Name": "Student 1",
+          "Total Modules Completed": 4,
+          "Overall Accuracy Rate (%)": 85.5,
+          "Overall Time Spent (minutes)": 450,
+          "Progress Percentage (%)": 80.0
+      },
+      "module_wise_performance": [
+          {
+              "Module ID": 1,
+              "Module Name": "Module 1",
+              "Module Completion Status": "Completed",
+              "Quiz Score": 90,
+              "Quiz Accuracy Rate (%)": 90,
+              "Time Taken for Quiz (minutes)": 30
+          },
+          {
+              "Module ID": 2,
+              "Module Name": "Module 2",
+              "Module Completion Status": "In Progress",
+              "Quiz Score": 80,
+              "Quiz Accuracy Rate (%)": 80,
+              "Time Taken for Quiz (minutes)": 25
+          }
+      ]
+  })
+  }
   const mockReview = {
     mistakes: [
       {
@@ -47,7 +77,7 @@ const Dashboard = () => {
       <TestList tests={mockTests} />
       <ScoreCard scores={mockScores} />
       <RecommendationBox recommendations={mockRecommendations} />
-      <button className=" w-[250px] h-[40px] bg-blue-500 text-md font-semibold p-2 rounded-xl text-white mt-16 flex justify-center align-middle items-center">Generate Report</button>
+      <button onClick={showReport} className=" w-[250px] h-[40px] bg-blue-500 text-md font-semibold p-2 rounded-xl text-white mt-16 flex justify-center align-middle items-center">Generate Report</button>
   
      
     </div>
